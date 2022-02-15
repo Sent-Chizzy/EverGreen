e96c70b2444025dad5354c248ce7e0dcc57216b1
Sent-Chizzy <godsentukomadu@outlook.com>
2022-02-15 15:52:40 +0100

    initial commit

---
+++ EvergreenMovies/EvergreenMovies/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvergreenMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Movies()
+        {
+            ViewBag.Message = "Your movies  page.";
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult WriteForUs()
+        {
+            ViewBag.Message = "Fill for to write for us";
+
+            return View();
+        }
+
+        public ActionResult Requestforlink()
+        {
+            ViewBag.Message = "Fill the form to get a torrent download link";
+
+            return View();
+        }
+    }
+}
